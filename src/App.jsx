43f6776925335049cb86ffb6de2b358115a0cd26
PaulDiaz054 <pd1004405054@gmail.com>
2026-02-21 17:13:06 -0500

    agregar dependencias para vercel Analytics and vercel Speed

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import MenuScreen from "./components/MenuScreen";
 import LobbyScreen from "./components/LobbyScreen";
 import PlayingScreen from "./components/PlayingScreen";
 import ResultsScreen from "./components/ResultsScreen";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const VerdaderosReales = () => {
   const gameRoom = useGameRoom();
 
   return (
     <div className="font-sans">
+      <Analytics />
+      <SpeedInsights />
       {gameRoom.gameState === "menu" && <MenuScreen {...gameRoom} />}
       {gameRoom.gameState === "lobby" && <LobbyScreen {...gameRoom} />}
       {gameRoom.gameState === "playing" && <PlayingScreen {...gameRoom} />}
